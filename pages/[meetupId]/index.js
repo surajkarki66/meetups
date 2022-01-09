b1bb39b2fa8cdea47cce75abb72cbfacbb5e8296
surajkarki <suraj.karki500@protonmail.com>
2022-01-09 22:40:21 +0545

    Modified pages

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,13 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      title="A first meetup"
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Claw_Fountain_at_Stanford_Univerisity.JPG/1280px-Claw_Fountain_at_Stanford_Univerisity.JPG"
+      address="Standford 5, 12345 California"
+      description="This is our first meetup."
+    />
+  );
+}
+export default MeetupDetails;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,34 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A first meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Claw_Fountain_at_Stanford_Univerisity.JPG/1280px-Claw_Fountain_at_Stanford_Univerisity.JPG",
+    address: "Standford 5, 12345 California",
+    description: "This is our first meetup.",
+  },
+  {
+    id: "m2",
+    title: "A second meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Claw_Fountain_at_Stanford_Univerisity.JPG/1280px-Claw_Fountain_at_Stanford_Univerisity.JPG",
+    address: "Standford 5, 12345 California",
+    description: "This is our second meetup.",
+  },
+  {
+    id: "m3",
+    title: "A third meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Claw_Fountain_at_Stanford_Univerisity.JPG/1280px-Claw_Fountain_at_Stanford_Univerisity.JPG",
+    address: "Standford 5, 12345 California",
+    description: "This is our third meetup.",
+  },
+];
+
 function HomePage() {
-  return <h1>Home</h1>;
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 }
 
 export default HomePage;
