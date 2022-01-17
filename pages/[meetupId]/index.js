a6339302368151ab36b15c1bfcccdf5581a54ae9
surajkarki66 <suraj.karki500@protonmail.com>
2022-01-17 16:42:29 +0545

    Fixed fallback

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -33,7 +33,7 @@ export async function getStaticPaths() {
 
   /* Defining Dynamic parameter value */
   return {
-    fallback: false,
+    fallback: "blocking",
     paths: meetups.map((meetup) => ({
       params: { meetupId: meetup._id.toString() },
     })),
